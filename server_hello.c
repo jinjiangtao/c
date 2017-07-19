2d65bc92ef160280135e833c7fbd8572ec6e0112
jinjiangtao <840129243@qq.com>
2017-07-19 16:49:24 +0800

    add server_hello.c

---
+++ server_hello.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+void error_handing(char *message);
+
+int main(int argc, char *argv[])
+{
+    int serv_sock;    
+    int clnt_sock;
+    
+    struct sockaddr_in serv_addr;
+    struct sockaddr_in clnt_addr;
+    socklen_t clnt_addr_size;
+
+    char message[] = "hello word!";
+
+    if(argc != 2){
+        printf("Usage");
+        exit(1);
+    }
+
+    //socket
+    serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+    if(serv_sock == -1){
+        error_handing("socket() err");
+    }
+     
+    //set params 
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET; 
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(atoi(argv[1]));
+
+    //bind
+    if(bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr))==-1){
+        error_handing("bind( err)");
+    }
+   
+    //listen
+    if(listen(serv_sock, 5) == -1){
+        error_handing("listen() err"); 
+    }
+    
+    //accept from serv_sock
+    clnt_addr_size = sizeof(clnt_addr);    
+    clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
+    if(clnt_sock == -1){
+        error_handing("accept() err");
+    }
+    
+    //write meeage and close
+    write(clnt_sock, message, sizeof(message));
+    close(clnt_sock); 
+    close(serv_sock);
+    return 0;
+}
+
+void error_handing(char *message)
+{
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
+
+
+
+
+
+
+
+
